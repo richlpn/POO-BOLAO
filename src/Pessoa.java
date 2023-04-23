71f26734c3152948e0cdd46230b4c3a532038ca2
Richard l <richard.nunes@morpheusjr.com>
2023-04-23 15:19:06 -0300

    Finalizado Construtor Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Pessoa {
+
+    protected String nome;
+    protected String cpf;
+    public Pessoa() {
+        Scanner input = new Scanner(System.in);
+        String confirm = "S";
+
+        while(confirm.equals("S")){
+            System.out.print("-> Digite o nome da pessoa: ");
+            nome = input.next();
+            System.out.print("-> Digite o CPF de(a) " + nome + ": ");
+            cpf = input.next();
+            System.out.println("-> "+ nome +" é o nome correto? Digite S para confirmar: ");
+            confirm = input.next();
+        }
+    }
+
 }
