625bbf5b11ae34f2898c5760abc7b7bf9f12cc79
Richard l <richard.nunes@morpheusjr.com>
2023-04-24 20:40:42 -0300

    Aposta.java inserirNumeros completo

--- src/Aposta.java
+++ src/Aposta.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Aposta {
     private ArrayList<Integer> numeros;
@@ -13,5 +14,29 @@ public class Aposta {
 
     public boolean vencedora(ArrayList<Integer> vencedores){
         return numeros.containsAll(vencedores);
+    }
+
+    public void inserirNumeros(){
+        Scanner scanner = new Scanner(System.in);
+        int quantApostas = -1, numAposta = -1;
+
+        while(quantApostas < 0){
+            System.out.println("-> Digite a quantidade de numeros que deseja apostar (> 0): ");
+            quantApostas = scanner.nextInt();
+        }
+
+
+        //Adciona os N numeros apostados entre 1 e 60
+        while(quantApostas > 0){
+            System.out.println("-> Digite um numero entre 1 e 60 para apostar: ");
+            numAposta = scanner.nextInt();
+
+            //Numero só é valido se não estiver sido apostado e se estiver entre 1 e 60
+            if(!numeros.contains(numAposta) && numAposta > 0 && numAposta <= 60){
+                quantApostas--;
+                numeros.add(numAposta);
+            }
+        }
+
     }
 }
